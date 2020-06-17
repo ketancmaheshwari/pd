06126edf4722c65692fdddd1d74d07216d123f0a
ketancmaheshwari <ketancmaheshwari@gmail.com>
2020-06-17 19:55:38 -0400

    extension to dir

--- pd.sh
+++ pd.sh
@@ -3,16 +3,18 @@
 sid=$(openssl rand -base64 16 | tr -dc 'a-z0-9' | head -c 6)
 
 echo $* | tr '|' '\n' | awk -v sid="$sid" 'BEGIN { 
-                                             printf("#!/bin/bash\n"); printf("mkdir %s\n",sid) 
+                                             printf("#!/bin/bash\n"); printf("mkdir %s.dir\n",sid) 
                                            } 
                                            NR == 1 { 
-                                             printf("%s > ./%s/%s.out 2> ./%s/%s.err \n echo %s. Exit status of %s is $?\n", $0, sid, NR, sid, NR, NR, $0) 
+                                             printf("%s > ./%s.dir/%s.out 2> ./%s.dir/%s.err \n echo %s. Exit status of %s is $?\n", 
+                                                    $0, sid, NR, sid, NR, NR, $0) 
                                            } 
                                            NR>1 { 
-                                             printf("%s < ./%s/%s.out > %s/%s.out 2> ./%s/%s.err \n echo %s. Exit status of %s is $?\n", $0, sid, NR-1, sid, NR, sid, NR, NR, $0) 
+                                             printf("%s < ./%s.dir/%s.out > %s.dir/%s.out 2> ./%s.dir/%s.err \n echo %s. Exit status of %s is $?\n", 
+                                                    $0, sid, NR-1, sid, NR, sid, NR, NR, $0) 
                                            } 
                                            END {
-                                             printf("echo The stdouts and stderrs are in ./%s dir\n",sid) 
+                                             printf("echo The stdouts and stderrs are in ./%s.dir dir\n",sid) 
                                            }' > $sid.sh
 
 
